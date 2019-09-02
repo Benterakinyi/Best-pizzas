ee2e2aa2e4477354b2838e70a64fc315df2ba87b
Benterakinyi <benterakinyi80@gmail.com>
2019-09-02 11:08:50 +0300

    add js/

---
+++ js/scripts.js
@@ -0,0 +1,99 @@
+// $(document).ready(function(){
+// $(".submit").click(function(){
+//  var name=$(".form").val();
+//  var number=$(".form").val();
+//  var email=$(".form").val();
+//  var adress=$(".form").val();
+//  var sizeofpizza=$(".form").val();
+//  var kindofcrust=$(".form").val();
+//  var typeoftopping=$(".form").val();
+//  var deliveryoption=$(".form").val();
+//  var wheretodeliver=$(".form").val();
+//  var totalcharge=$(".form").val();
+//  if((name !=="") && (number !=="") && (email !=="") && (adress !=="") && (sizeofpizza !=="") && (kindofcrust !=="" ) && (typeoftopping !=="") && (deliveryoption !=="") && (wheretodeliver !=="") && (totalcharge !=="")){
+//   alert(name + "," + "your order is - and the total charge is-")
+//  }
+//  else{
+//      alert("kindly fill the form")
+//  }
+// })
+// });
+//business logic
+function Order(type, size, crust, topping) {
+    this.type = type;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+ }
+ 
+ Order.prototype.fullOrder = function () {
+    return this.type + " with the crust of " + this.crust + " and " + this.topping + " as topping.";
+ };
+ 
+ function Total(price, quantity, delivery) {
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
+ }
+ 
+ Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+ };
+ 
+ 
+ var sizePrice = [1200, 900, 600]
+ var deliverPrices = [0, 200];
+ //user interface logic
+ $(document).ready(function () {
+    $('form#myForm').submit(function (event) {
+        event.preventDefault();
+        var pizzaType = $('#type').val();
+ 
+        var pizzaSize = parseInt($('#size').val());
+ 
+        var pizzaCrust = $('#crust').val();
+ 
+        var pizzaTop = $('#top').val();
+ 
+        var pizzaQty = parseInt($('#qty').val());
+ 
+        var pizzaPick = parseInt($('#pick').val());
+ 
+ 
+        var price = sizePrice[pizzaSize - 1];
+ 
+ 
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
+ 
+ 
+ 
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
+        alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+        alert("your bill is: " + newTotal.finalTotal());
+        }else{
+            if(pizzaPick===2){
+                prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal());
+            }
+        }
+ 
+    });
+ 
+ 
+ 
+ 
+    //sending message function
+    $('form#contactForm').submit(function (event) {
+        event.preventDefault();
+        var name = $('#name').val();
+        var pass = $('#email').val();
+        var mess = $('#mess').val();
+        alert("Hi " + name + " we have received your message and we will get in touch. Thank you for contacting us.");
+    });
+ 
+ 
+ });
